acaf5f97ffe79e21a9d3581c68ed103655dcbb20
bensutherland <bensutherland7@gmail.com>
2016-10-13 15:35:48 -0400

    Improve chr-wide to use perms correctly

--- 01_scripts/02_permutations_pQTL_2016-03-14.R
+++ 01_scripts/02_permutations_pQTL_2016-03-14.R
@@ -55,9 +55,8 @@ operm.im <- scanone(sfon, addcovar=sex, intcovar=sex, n.perm=num.perm,
 #note: at ~ 100perms/2h; therefore to scale make sure to use n.cluster
 
 
-## Chromosome-wide significance using for loop for pheno and chr, 
+## 2C Chromosome-wide scanone and permutations ####
 ## includes if/else statement to use the covariate of sex when required
-## collects scanone objects and the chromosome-level p = (chr.wide.pval) sig LOD
 
 # select your phenotypes and chromosomes
 all.phenos <- c(names(ph.no.cov), ph.sex.sp, names(ph.yes.cov))
@@ -66,50 +65,42 @@ selected.chrs <- 1:42
 # set your pval
 chr.wide.pval <- 0.01
 
-# set NULLs
-chr.scan = NULL # a scanone object rewritten each chromosome
-chr.scan.perm = NULL # scanone object with permutations rewritten each chromosome
-chr.sig = NULL # this is the LOD level for each chromosome at p = 0.05
-scanone.mods <- list() # this is a list of scanone objects, one for each pheno x chr
-pheno.names = NULL # this is a vector of the names of tested phenotypes
-pheno.sig.lod.per.chr = NULL # this is the LOD sig level for each pheno x chr
+# find which phenotypes are numeric
+nums <- sapply(sfon$pheno, is.numeric) # find out which phenotypes to assess (are numeric)
 
-# Loop across phenos, and chromosomes
-for(pheno in all.phenos) {
-  chr.sig = NULL # set NULL each pheno loop
-  for(chr in selected.chrs) {
-if (pheno %in% names(ph.yes.cov)) {
-  # use covariate if needed
-  chr.scan <- scanone(sfon, method = "hk", pheno.col=pheno, chr=chr, addcov=sex)
-  chr.scan.perm = scanone(sfon, method="hk", 
-                          pheno.col=pheno, chr=chr, addcov=sex,
-                          n.perm=num.perm, n.cluster = num.cluster 
-                          , verbose=T)
-  chr.sig = c(chr.sig, summary(chr.scan.perm, chr.wide.pval))
-  scanone.mods[[paste(pheno, "_", chr, sep="")]] <- chr.scan
-} else {
-  # don't use covariate
-  chr.scan <- scanone(sfon, method = "hk", pheno.col=pheno, chr=chr)
-  chr.scan.perm = scanone(sfon, method="hk", 
-                          pheno.col=pheno, chr=chr, 
-                          n.perm=num.perm, n.cluster = num.cluster 
-                          , verbose=T)
-  chr.sig = c(chr.sig, summary(chr.scan.perm, chr.wide.pval))
-  scanone.mods[[paste(pheno, "_", chr, sep="")]] <- chr.scan
-}
-}
-pheno.names = c(pheno.names, names(sfon$pheno[pheno])) # obtain the name of the tested pheno in this loop
-pheno.sig.lod.per.chr = cbind(pheno.sig.lod.per.chr, chr.sig)
-}
-colnames(pheno.sig.lod.per.chr) <- pheno.names #gives names to the average significance levels
-pheno.sig.lod.per.chr
-scanone.mods
+print(paste(c("Using", num.perm, "permutations")), quote=F)
+
+results <- capture.output(
+  for(m in which(nums)) {
+    x.hk <- NULL
+    x.perm <- NULL
+    i=NULL
+    print(paste(c(m, names(sfon$pheno[m])))) # print the phenotype being tested
+    if(names(nums[m]) %in% c(names(ph.no.cov), ph.sex.sp)) {
+      print("Not incl covariate") # do not use covariate
+      for(i in 1:nchr(sfon)){
+        x.hk=scanone(sfon, method="hk", pheno.col=m, chr=i)
+        x.perm=scanone(sfon, method="hk", n.perm=num.perm, n.cluster=num.cluster, pheno.col=m, chr=i, verbose=F)
+        print(paste(c("Testing chromosome", i))) # print the chromosome being tested 
+        print(summary(x.hk, perms=x.perm, alpha=0.01, pvalues=T, format="tabByCol")) #find the LOD peak for this
+      }
+    }
+    else if(names(nums[m]) %in% names(ph.yes.cov)){
+      print("Including covariate")
+      for(i in 1:nchr(sfon)){
+        x.hk=scanone(sfon, method="hk", pheno.col=m, chr=i, addcov=sex)
+        x.perm=scanone(sfon, method="hk", n.perm=num.perm, n.cluster=num.cluster, pheno.col=m, chr=i, addcov=sex, verbose=F)
+        print(paste(c("Testing chromosome", i))) # print the chromosome being tested 
+        print(summary(x.hk, perms=x.perm, alpha=0.01, pvalues=T, format="tabByCol")) #find the LOD peak for this
+      }
+    }
+  }
+)
 
 
-str(scanone.mods)
+write.csv(results, file = "new_chr_wide_p0.01_w_10000perms.csv", quote=F, row.names=F)
 
-
-#####2C SEX AS A BINARY TRAIT####
+#####2D SEX AS A BINARY TRAIT####
 sfon$pheno <- cbind(sfon$pheno, binary=sex)
 out.bin <- scanone(sfon, pheno.col="binary",
                    model="binary")
@@ -117,23 +108,6 @@ out.bin <- scanone(sfon, pheno.col="binary",
 # Genome-wide significance
 operm.bin <- scanone(sfon, pheno.col="binary", 
                      model="binary", n.perm=num.perm, n.cluster = num.cluster)
-
-
-#####3D INTERVAL ESTIMATES#####
-#?bayesint # uses output from scanone
-#use in C Sauvage's analysis: 
-# bayesint(outall.hk, 5, 0.95, expandtomarkers=T) # chr = 5, prob=0.95, allow interval to flanking markers
-# out.boot = scanoneboot(raw.data, chr = 1:40, n.boot = 100) #cross = 'raw.data'
-#?scanoneboot # get estimated confidence interval for the location of a QTL
-
-#Try 1.5 LOD interval
-
-# Histogram of the estimated QTL locations in 1000 bootstrap replicates
-#hist(out.boot, main="Bootstrap values of QTL position", xlab="Estimated QTL location (cM)",
-#     ylab="", yaxt="n")
-
-#rug(raw.data$geno[[5]]$map) # not sure what this is showing,
-
 
 ### SAVE OUT PERMUTATION OBJECT ####
 #save.image(file = "sfon_02_output.RData") # save out existing data so that it can be reloaded without running all again
