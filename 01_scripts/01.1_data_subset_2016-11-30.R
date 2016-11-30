5520c42b84a9a4251d61df76c85b2b267ff3aaab
bensutherland <bensutherland7@gmail.com>
2016-11-30 14:19:06 -0500

    Include option to subset sexes for tests

--- 01_scripts/01.1_data_subset_2016-11-30.R
+++ 01_scripts/01.1_data_subset_2016-11-30.R
@@ -14,11 +14,14 @@ setwd("~/Documents/bernatchez/01_Sfon_projects/03_Sfon_pQTL/sfon_pqtl")
 # Load Part 1 results:
 load("02_data/sfon_01_output.RData")
 
+
+#### REMOVE SPECIFIC MARKER TYPES ####
 # Use marker list to remove markers
 sfon_remove <- read.csv(file = "02_data/sfon_efxeg_and_hkxhk_markers.csv", header = F)
 sfon_remove <- as.numeric(sfon_remove)
-length(sfon_remove) # 1208 markers
+length(sfon_remove) # 1208 markers to remove
 sfon_only_nnxnp <- drop.markers(sfon, sfon_remove) # drop markers
+rm(sfon_remove)
 # note that some may already be removed due to seg distortion filter
 
 summary(sfon_only_nnxnp) #2509 markers
@@ -26,5 +29,16 @@ plot.map(sfon_only_nnxnp, alternate.chrid = T,
          main = "", xlab = "Linkage Group")
 text(x = 35, y = 260, labels = paste("n =", sum(nmar(sfon_only_nnxnp)), "markers"))
 
+##export subset data
+# save.image(file = "02_data/sfon_01_output_subset_only_nnxnp.RData")
+
+#### REMOVE SPECIFIC INDIV SEX ####
+# Identify male and female individuals
+ind.males = c(sfon_only_nnxnp$pheno$sex=="M")
+ind.females = c(sfon_only_nnxnp$pheno$sex=="F")
+
+sfon.males <- subset(sfon_only_nnxnp, ind=ind.males)
+sfon.females <- subset(sfon_only_nnxnp, ind=ind.females)
+
 #export subset data
-save.image(file = "02_data/sfon_01_output_subset_only_nnxnp.RData")
+save.image(file = "02_data/sfon_01_output_subset_only_nnxnp_and_mf_sep.RData")
