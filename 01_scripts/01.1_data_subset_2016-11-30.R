5ab5b2d0cc022ab2117f974598be76c457fe1675
bensutherland <bensutherland7@gmail.com>
2016-11-30 14:04:45 -0500

    Create script for subsetting marker type

---
+++ 01_scripts/01.1_data_subset_2016-11-30.R
@@ -0,0 +1,30 @@
+# Subset R/qtl object for marker type or for individual sex
+# this assumes you have made marker lists for the marker types hkxhk and efxeg to remove (see readme.md)
+# by doing
+# grep -E 'efxeg|hkxhk' Sfon_female_map_v4.3.loc | awk 'BEGIN{ORS=","}1 { print $1 }' | sed 's/,$//g' > sfon_efxeg_and_hkxhk_markers.csv
+
+# rm(list=ls())
+
+# install libraries
+require(qtl)
+
+# set working directory
+setwd("~/Documents/bernatchez/01_Sfon_projects/03_Sfon_pQTL/sfon_pqtl")
+
+# Load Part 1 results:
+load("02_data/sfon_01_output.RData")
+
+# Use marker list to remove markers
+sfon_remove <- read.csv(file = "02_data/sfon_efxeg_and_hkxhk_markers.csv", header = F)
+sfon_remove <- as.numeric(sfon_remove)
+length(sfon_remove) # 1208 markers
+sfon_only_nnxnp <- drop.markers(sfon, sfon_remove) # drop markers
+# note that some may already be removed due to seg distortion filter
+
+summary(sfon_only_nnxnp) #2509 markers
+plot.map(sfon_only_nnxnp, alternate.chrid = T,
+         main = "", xlab = "Linkage Group")
+text(x = 35, y = 260, labels = paste("n =", sum(nmar(sfon_only_nnxnp)), "markers"))
+
+#export subset data
+save.image(file = "02_data/sfon_01_output_subset_only_nnxnp.RData")
