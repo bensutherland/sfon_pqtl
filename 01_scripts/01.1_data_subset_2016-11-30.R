6b35a60c09570b41a9376584a1c78865c9312e72
bensutherland <bensutherland7@gmail.com>
2017-01-30 14:41:10 -0500

    Remove the non-used marker removal options, only retain the final good set

--- 01_scripts/01.1_data_subset_2016-11-30.R
+++ 01_scripts/01.1_data_subset_2016-11-30.R
@@ -1,7 +1,7 @@
 # Subset R/qtl object for marker type or for individual sex
 # this assumes you have made marker lists for the marker types hkxhk and efxeg to remove (see readme.md)
 # by doing
-# grep -E 'efxeg|hkxhk' Sfon_female_map_v4.3.loc | awk 'BEGIN{ORS=","}1 { print $1 }' | sed 's/,$//g' > sfon_efxeg_and_hkxhk_markers.csv
+# 
 
 # rm(list=ls())
 
@@ -17,16 +17,13 @@ load("02_data/sfon_01_output.RData")
 
 #### REMOVE SPECIFIC MARKER TYPES ####
 # Choose which removal set
-#sfon_remove <- read.csv(file = "02_data/sfon_efxeg_and_hkxhk_markers.csv", header = F) # keep only nnxnp
-#sfon_remove <- read.csv(file = "02_data/sfon_nnxnp_and_hkxhk_markers.csv", header = F) # keep only efxeg
-#sfon_remove <- read.csv(file = "02_data/sfon_efxeg_markers.csv", header =F) # keep nnxnp and hkxhk
-#sfon_remove <- read.csv(file = "02_data/sfon_nnxnp_markers.csv", header = F)
+sfon_remove <- read.csv(file = "02_data/sfon_nnxnp_markers.csv", header = F) # to remove nnxnp markers
 sfon_remove <- as.numeric(sfon_remove)
+str(sfon_remove)
 
 length(sfon_remove) # markers to remove
-sfon_limited <- drop.markers(sfon, sfon_remove) # drop markers
+sfon_limited <- drop.markers(sfon, sfon_remove) # drop markers # note that some may already be removed due to seg distortion filter
 rm(sfon_remove)
-# note that some may already be removed due to seg distortion filter
 
 summary(sfon_limited) # check markers
 plot.map(sfon_limited, alternate.chrid = T,
@@ -38,10 +35,7 @@ text(x = 35, y = 260, labels = paste("n =", sum(nmar(sfon_limited)), "markers"))
 # sfon <- est.rf(sfon) #doesn't seem to be necessary for crossover determination or for cM position
 
 ##export subset data
-# save.image(file = "02_data/sfon_01_output_subset_only_nnxnp.RData")
-# save.image(file = "02_data/sfon_01_output_subset_only_nnxnp_and_hkxhk.RData")
 save.image(file = "02_data/sfon_01_output_subset_only_efxeg_and_hkxhk.RData") # to use for heterochiasmy
-
 
 
 #### REMOVE SPECIFIC INDIV SEX ####
